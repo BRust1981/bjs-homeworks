46da5548133c84d3a1c108d614fac1caeba027f2
Rustam Barlybaev <bargus@rambler.ru>
2019-07-13 12:48:28 +0300

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Задача 1
+let algebra;
+let geography;
+let physics;
+
+algebra = 4;
+geography = 4;
+physics = 5;
+
+let avgRating = Math.trunc((algebra + geography + physics) / 3);
+
+console.log(`Average rating:  ${avgRating}`);
+
+// Задача 2
+
+let name = 'Рустам';
+
+let message = `Привет, мир! Меня зовут ${name}.`;
+console.log(message);
+
+// Задача 3
+
+let x; 
+let y;
+let z;
+
+x = 2;
+y = 22;
+z = 0;
+
+let result = x * y + 5 * z + x - 1;
+console.log(`Result: ${result}`);
