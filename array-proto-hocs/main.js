082eb587ca2da74386509f9429ca36d8a520ae8e
BRust1981 <42816397+BRust1981@users.noreply.github.com>
2019-07-26 00:50:38 +0300

    second commit array-proto-hocs

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -7,6 +7,8 @@ function compareArrays(){
   const args = Array.from(arguments);
   if(Array.from(args).some(param => !Array.isArray(param))){
     throw new Error('Аргументы должны быть массивами!');
+  } else if(args.length < 2){
+    throw new Error('Для сравнения требуется минимум 2 массива!');
   } else {
     if(args.some(param => param.length !== args[1].length)){
       return false;
@@ -24,25 +26,31 @@ function compareArrays(){
 function memoize(fn, limit){
   const results = [];
   return function(){
+    // входные параметры
     let arrArgs = Array.from(arguments);
+    // ищем эти параметры в расчитанных ранее
     let existParam = results.find((savedParams) => {
-        return savedParams.args.every((arr, index) => {
+        return savedParams.args.length === arrArgs.length && savedParams.args.every((arr, index) => {
           return compareArrays(arr, arrArgs[index]);
         });
     });
+    // Если параметр ранее был расчитан и сохранен в памяти
     if(existParam !== undefined){
       console.log(`Результат из памяти: "${existParam.result}".`);
       return existParam.result;
-    } else {
+    } else {    // иначе создаем новый расчет
       const newResult = {
         args: arrArgs,
         result: compareArrays(...arguments)
       };
-
+      // проверяем не полон ли массив результатов
       if(results.length >= limit){
+        // если ДА, то удаляем один результат в начале
         results.shift();
       } 
+      // и добавляем новый в конец
       results.push(newResult);
+      // сообщаем о результатах
       console.log(`Результат ${newResult.result} расчитан и сохранен в памяти.`);
       return newResult.result;
     }
